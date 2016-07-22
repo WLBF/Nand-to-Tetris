f2ed73604ce34895263c5042a708c280cdb6df94
WLBF <lbf1353@live.com>
2016-07-23 00:00:28 +0800

    Update project4b

--- nand2tetris/projects/04/fill/Fill.asm
+++ nand2tetris/projects/04/fill/Fill.asm
@@ -9,3 +9,78 @@
 // program clears the screen, i.e. writes "white" in every pixel.
 
 // Put your code here.
+
+    @i
+    M=0
+
+(LOOP)
+
+// If any key pressed goto BLACKEN
+    @24576
+    D=M
+
+    @BLACKEN
+    D;JGT
+
+(CLEAR)
+
+// If i<=0 goto LOOP
+    @i
+    D=M
+
+    @LOOP
+    D;JLE
+//
+
+// Decrement i then clear the RAM[SCREEN+i]
+    @i
+    M=M-1
+
+    @SCREEN
+    D=A
+
+    @i
+    D=D+M
+
+    A=D
+    M=0
+
+    @LOOP
+    0;JMP
+//
+
+(BLACKEN)
+
+// If i > 8191 goto LOOP
+    @i
+    D=M
+
+    @8191
+    D=D-A
+
+    @LOOP
+    D;JGT
+//
+
+// Blacken RAM[SCREEN+i]
+    @SCREEN
+    D=A
+
+    @i
+    D=D+M
+
+    A=D
+    M=!M
+//
+
+// Increment i then goto LOOP
+    @i
+    M=M+1
+
+    @LOOP
+    0;JMP
+//
+
+
+
+
